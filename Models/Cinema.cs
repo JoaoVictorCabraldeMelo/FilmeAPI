bb2ddb206d74f2f0f5742432ec5dff49f83da266
Joao Victor Cabral de Melo <joao.victor.cabralmelo@gmail.com>
2023-12-20 18:30:37 -0300

    add: cinema

---
+++ Models/Cinema.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmeAPI;
+
+public class Cinema
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O campo do nome e obrigatorio")]
+    public string Nome { get; set; }
+}
---
+++ Models/Dtos/CinemaDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmeAPI;
+
+public class CinemaDto
+{
+  [Required(ErrorMessage = "O campo do nome e obrigatorio")]
+  public string Nome { get; set; }
+}
